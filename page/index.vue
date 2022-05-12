316c5caa12193938eb21047871ed353bc8db6970
suixuan287 <287996084@qq.com>
2022-05-12 15:34:33 +0800

    feat: add all

---
+++ page/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+        <h1>Hello, this is BMC</h1>
+  </div>
+</template>
+<script>
+import './index.less';
+import { defineComponent } from 'vue';
+export default defineComponent ({
+    name: 'App',
+    setup() {
+
+    }
+})
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const fs = require("fs");
+
+const config = {
+    entry: {},
+    devtool: "source-map",
+    mode: "development",
+    output: {
+        filename: "[name].bundle.[hash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        port: 8090,
+        open: true,
+        compress: false
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+        new VueLoaderPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: ["vue-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "dist",
+                        },
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "dist",
+                        },
+                    },
+                    {
+                        loader: "css-loader",
+                        options: { url: false, sourceMap: true },
+                    },
+                    { loader: "less-loader", options: { sourceMap: true } },
+                ],
+            },
+        ],
+    },
+};
+
+const entry = {};
+const htmlTemplates = [];
+const jsFiles = fs.readdirSync(__dirname + "/src/pages");
+jsFiles.forEach((item) => {
+    const pageName = item;
+    htmlTemplates.push(
+        new HtmlWebpackPlugin({
+            title: pageName,
+            filename: `${pageName}.html`,
+            chunks: [`${pageName}`],
+            inject: 'body',
+            template: path.resolve(__dirname, "template/pc.template.html"),
+        })
+    );
+    entry[pageName] = `./src/pages/${pageName}/index.js`;
+});
+config.entry = entry;
+config.plugins.push(...htmlTemplates);
+
+module.exports = config;
